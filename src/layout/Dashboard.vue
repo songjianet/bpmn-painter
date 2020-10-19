d2431b5682646084077a2d8c86bfbe595738daff
songjian <1778651752@qq.com>
2020-10-19 21:46:04 +0800

    feat: 能够在项目中创建出绘制画布

--- src/layout/Dashboard.vue
+++ src/layout/Dashboard.vue
@@ -2,10 +2,7 @@
   <div class="container">
     <b-p-m-n-header></b-p-m-n-header>
     <div class="content">
-      <b-p-m-n-left-pane></b-p-m-n-left-pane>
-      <div class="canvas">
-        <router-view></router-view>
-      </div>
+      <div class="canvas" ref="canvas"></div>
       <b-p-m-n-right-pane></b-p-m-n-right-pane>
     </div>
   </div>
@@ -13,14 +10,39 @@
 
 <script>
 import BPMNHeader from '@/components/Header'
-import BPMNLeftPane from '@/components/LeftPane'
 import BPMNRightPane from '@/components/RightPane'
+// import bpmnData from '@/assets/js/bpmnData'
+import defaultXML from '@/assets/js/defaultXML'
+import Modeler from 'bpmn-js/lib/Modeler'
 
 export default {
   name: 'Dashboard',
+  data() {
+    return {
+      modeler: null,
+      xml: ''
+    }
+  },
+  mounted() {
+    this.modeler = new Modeler({
+      container: this.$refs.canvas
+    })
+
+    !this.xml ? this.init(defaultXML()) : this.init(this.xml)
+  },
+  methods: {
+    async init(xml) {
+      xml = xml.replace(/<!\[CDATA\[(.+)]]>/g, '&lt;![CDATA[$1]]&gt;')
+
+      try {
+        await this.modeler.importXML(xml)
+      } catch (err) {
+        console.error(err.message, err.warnings)
+      }
+    }
+  },
   components: {
     BPMNHeader,
-    BPMNLeftPane,
     BPMNRightPane
   }
 }
@@ -37,7 +59,7 @@ export default {
   justify-content: center;
 
   .canvas {
-    width: calc(100% - 400px);
+    width: calc(100% - 300px);
   }
 }
 </style>
