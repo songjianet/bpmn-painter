bc2381be0942a8e9886f95e4e107e9dac98e37d3
songjian <1778651752@qq.com>
2020-10-29 16:13:18 +0800

    feat: 增加xml去空格和回车的功能

--- utils/drawToXML.js
+++ utils/drawToXML.js
@@ -4,8 +4,13 @@
  * */
 
 export const drawToXML = (xml) => {
-  xml = xml.replace(/&lt;/g, '<')
-  xml = xml.replace(/&gt;/g, '>')
+  // xml = xml.replace(/&lt;/g, '<')
+  // xml = xml.replace(/&gt;/g, '>')
+  xml = xml.replace(/(\n)/g, '')
+  xml = xml.replace(/(\t)/g, '')
+  // xml = xml.replace(/(\r)/g, '')
+  xml = xml.replace(/>\s*</g, '><')
+  // xml = xml.replace(/ /ig, ' ')
 
   return xml
 }
