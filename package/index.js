b8b5f878965950ab4b5d0a5e58e59737a2c53e0b
songjian <1778651752@qq.com>
2020-10-30 14:34:10 +0800

    fix: 解决无法在组件内单独使用的问题

--- package/index.js
+++ package/index.js
@@ -1,25 +1,37 @@
+// import BpmnPainter from './components/BpmnPainter'
+//
+// const components = [
+//   BpmnPainter
+// ]
+//
+// const install = Vue => {
+//   if (install.installed) return
+//   install.installed = true
+//   components.map(component => {
+//     Vue.component(component.name, component)
+//   })
+// }
+//
+// if (typeof window !== 'undefined' && window.Vue) {
+//   install(window.Vue)
+// }
+//
+// export default {
+//   install,
+//   ...components
+// }
+//
+// export {
+//   BpmnPainter
+// }
+
 import BpmnPainter from './components/BpmnPainter'
 
-const components = [
-  BpmnPainter
-]
-
-const install = Vue => {
-  if (install.installed) return
-  install.installed = true
-  components.map(component => {
-    Vue.component(component.name, component)
-  })
+BpmnPainter.install = Vue => {
+  Vue.component(BpmnPainter.name, BpmnPainter)
 }
 
-if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue)
-}
-
-export default {
-  install,
-  ...components
-}
+export default BpmnPainter
 
 export {
   BpmnPainter
